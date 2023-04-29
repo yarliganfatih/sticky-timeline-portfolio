d3a525b969c140834477cd7c22f4fc057aee647c
Fatih Yarlıgan <fatih.yarligan@bahcesehir.edu.tr>
2023-04-29 16:38:12 +0300

    multiple search method by a query

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App: React.FC = () => {
   const [items, setItems] = useState<Item[]>(data);
   const [searchTerm, setSearchTerm] = useState<SearchTerm>({});
 
-  const filterItems = (searchTerm: SearchTerm) => {
+  const andFilterItems = (searchTerm: SearchTerm) => {
     console.log(searchTerm);
     const filtered = data.filter((item: Item) => {
       let isMatch = true;
@@ -34,26 +34,65 @@ const App: React.FC = () => {
     setItems(filtered);
   };
 
+  const orFilterItems = (searchTerm: SearchTerm) => {
+    const filtered = data.filter((item: Item) => {
+      const matches = Object.entries(searchTerm).map(([key, value]) => {
+        if (value) {
+          return item[key]
+            .toString()
+            .toLowerCase()
+            .includes(value.toString().toLowerCase());
+        }
+        return true;
+      });
+      return matches.includes(true);
+    });
+    setItems(filtered);
+  };
+
+  const fillSearchTerm = (_value:string) => {
+    let _searchTerm: SearchTerm = {};
+    Object.entries(data[0]).forEach(([key]) => {
+      _searchTerm = {..._searchTerm, [key]: _value};
+    });
+    setSearchTerm(_searchTerm);
+    orFilterItems(_searchTerm);
+  };
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setSearchTerm((prevSearchTerm) => ({ ...prevSearchTerm, [name]: value }));
-    filterItems({ ...searchTerm, [name]: value });
+    andFilterItems({ ...searchTerm, [name]: value });
   };
 
   return (
     <>
-      <input
-        name="name"
-        type="text"
-        placeholder="Name"
-        onChange={handleChange}
-      />
-      <input
-        name="count"
-        type="number"
-        placeholder="Count"
-        onChange={handleChange}
-      />
+      <form>
+        <input
+          name="name"
+          type="text"
+          placeholder="Name"
+          onChange={handleChange}
+        />
+        <input
+          name="count"
+          type="number"
+          placeholder="Count"
+          onChange={handleChange}
+        />
+        <input
+          name="query"
+          type="text"
+          placeholder="Query"
+          onChange={(e) => { fillSearchTerm(e.target.value); }}
+        />
+        <input
+          name="reset"
+          type="reset"
+          value="Reset"
+          onClick={() => { fillSearchTerm(""); }}
+        />
+      </form>
       <ul>
         {items.map((item) => (
           <li key={item.id}>
